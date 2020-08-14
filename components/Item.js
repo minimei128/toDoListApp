4e88e1d5766b3d33784412ca09ae059c76cf0cd0
minimei128 <mphan2868@hotmail.com8>
2020-08-14 15:08:49 +1000

    add item components in app

---
+++ components/Item.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { 
+    Text, 
+    View, 
+    StyleSheet } from 'react-native';
+
+export const Item = (props) => {
+    return(
+        //render each item on the list
+        <View style = {itemStyles.item}> 
+            <Text style={itemStyles.text}>{props.item_name}</Text>
+        </View>
+    )
+}
+
+const itemStyles = StyleSheet.create({
+    item: {
+        padding: 10,
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    },
+    text: {
+        fontSize:16,
+        color: 'black'
+    }
+})
