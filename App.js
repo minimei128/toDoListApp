a9d3518fa60186ed8ec65d10d89f99daed3fec3b
minimei128 <mphan2868@hotmail.com8>
2020-08-14 17:30:02 +1000

    add text input, add button and listview in app

--- App.js
+++ App.js
@@ -1,21 +1,118 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React , {Component} from 'react';
+import { 
+  StyleSheet, 
+  Text, 
+  View, 
+  SafeAreaView,
+  FlatList,
+  TextInput,
+  Button,
+  TouchableOpacity } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  import {Item} from './components/Item';
+
+export default class App extends Component {
+
+  state = {
+    expenseAmount : 0,
+    expenseCategory: '',
+    updating: false,
+  }
+
+  listData = []
+
+  render() {
+    return (
+// start --- main app container 
+      <SafeAreaView>
+          {/* start --- text input container */}
+          <View style = {styles.main}>
+              
+              <Text>To Do List</Text>
+              
+               {/* section: enter/input amount on app */}
+              <TextInput
+                style={styles.input}
+                placeholder= "$ amount"
+                onChangeText={ text => this.setState({expenseAmount: parseFloat(text) }) }
+                keyboardType="number-pad" />
+              {/* section: enter/input category on app */}
+             <TextInput
+                style={styles.input}
+                placeholder="category"
+                onChangeText={ text => this.setState({expenseCategory: text})}
+            />
+        </View>
+        {/* end --- text input container */}
+
+        {/* start --- add button container */}
+        <View>
+            <TouchableOpacity style={styles.button} onPress={this.addItem}>
+            <Text style={styles.buttonText}>Add</Text>
+            </TouchableOpacity>
+        </View>
+        {/* end --- add button container */}
+
+        {/* User input item display in a list */}
+        <FlatList
+          data={this.listData}
+          renderItem={this.renderList}
+          keyExtractor={ item => item.id} 
+          extraData = {this.state.expenseAmount}
+          />
+
+      </SafeAreaView>
+      // end --- main app container 
+    )
+  }
+
+
+  renderList = ({item}) => (
+    <Item amount={item.amount} category={item.category} />
+  )
+
+  addItem = () => {
+    if(
+      isNaN(this.state.expenseAmount) ||
+      this.state.expenseAmount == 0 ||
+      this.state.expenseCategory == '') {
+        return;
+      }
+      let itemId = new Date().getTime().toString()
+      let listItem = {
+        id: itemId,
+        amount: this.state.expenseAmount,
+        category: this.state.expenseCategory
+      }
+      this.listData.push(listItem)
+      console.log('adding')
+      //this.setState({updating: true})
+      this.setState({expenseAmount:0})
+    }
+  }
+
+const colors = {
+      primary : 'hsla(330,38%,65%,1)'
+    }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  main: {
+    paddingHorizontal: 10,
   },
-});
+  input: {
+    width: '100%',
+    padding: 10,
+    borderColor: 'black',
+    borderWidth: 1,
+    marginVertical: 15
+  },
+  button: {
+    padding: 15,
+    backgroundColor: colors.primary
+  },
+  buttonText: {
+    color: 'white',
+    textAlign: 'center'
+  }
+})
--- components/Item.js
+++ components/Item.js
@@ -8,7 +8,8 @@ export const Item = (props) => {
     return(
         //render each item on the list
         <View style = {itemStyles.item}> 
-            <Text style={itemStyles.text}>{props.item_name}</Text>
+            <Text style={itemStyles.text}>{props.category}</Text>
+            <Text style={itemStyles.text}>{props.amount}</Text>
         </View>
     )
 }
